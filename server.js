e229e02a9568a1c19c912d350cea51269f3d696f
Kim <kim@MacBook-Pro-2.local>
2024-11-14 00:23:18 -0800

    updates

--- server.js
+++ server.js
@@ -157,7 +157,7 @@ app.post('/api/chat', async (req, res) => {
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on ${PORT}`);
 });
 
 /**************************************************************************
